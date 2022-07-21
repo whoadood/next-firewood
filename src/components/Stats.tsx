74177d471d61fe138740d9fbda7dc5cdd9472a00
Castle <castlerock88@protonmail.com>
2022-07-21 00:19:44 -0400

    updates

--- src/components/Stats.tsx
+++ src/components/Stats.tsx
@@ -1,11 +1,3 @@
-/* This example requires Tailwind CSS v2.0+ */
-import {
-  AnnotationIcon,
-  GlobeAltIcon,
-  LightningBoltIcon,
-  ScaleIcon,
-} from "@heroicons/react/outline";
-
 const stats = [
   {
     stat: "24/7",
